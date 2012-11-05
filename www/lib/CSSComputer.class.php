b3ea5b75b1d606aa790dddd6e17f02d178e5774d
LeChatLéon <contact@lechatleon.com>
2012-11-05 20:35:48 +0100

    CSSComputer now functional.

--- www/lib/CSSComputer.class.php
+++ www/lib/CSSComputer.class.php
@@ -13,18 +13,92 @@ class CSSComputer{
 	
 	public static function genCustomisation($stylesSettings){
 
+		
+		$font = $stylesSettings->style->font;
+		
+		self::addProperty('html', 'color', self::tabToRGB($font->color));
+		self::addProperty('a', 'color', self::tabToRGB($font->linkColor));
+		self::addProperty('a', 'border-color', self::tabToRGB($font->linkColor));
+		self::addProperty('a:hover', 'color', self::tabToRGB($font->color));
+		self::addProperty('a:hover', 'border-color', self::tabToRGB($font->color));
+		$defaultFont = "default".$font->defaultStyle;
+		self::addProperty('html', 'font-family', $font->$defaultFont);
+		self::addProperty('html', 'font-size', $font->defaultSize."px");
+		self::addProperty('html', 'text-align', $font->defaultAlignment);
+		
+		self::addProperty('article', 'line', $font->interline);
+		
+		self::addProperty('#background, html', 'background-color', self::tabToRGB($stylesSettings->style->backgroundColor));
+		
+		/*/ TODO Backgrounds /*/
+		
 		$block = $stylesSettings->style->blocks;
 
 		$margin = $block->margin;
 		$padding = $block->padding;
-		$oneUnit = $block->width; 
+		$oneUnit = $block->width; 		
+		$twoUnits = 2*($oneUnit+$padding+$margin);
+		$threeUnits = 4*$margin+4*$padding+3*$oneUnit;
 
 		$totalWidth = 3*(2*$margin+2*$padding+$oneUnit);
 
 		self::addProperty('section','width',$totalWidth."px");
+				
+		self::addProperty('article, header','background-color', self::tabToRGB($block->backgroundColor));
+		self::addProperty('article, header','border-radius', $block->borderRadius."px", true);
+		self::addProperty('article, header','box-shadow', self::tabToShadow($block->shadow), true);
+		if($block->alignment != "default")
+		self::addProperty('article','text-align', $block->alignment);
+		self::addProperty('article, header','margin', $block->margin."px");
+		self::addProperty('article, header','padding', $block->padding."px");		
+		
+		self::addProperty('.u1, .u1 img','width', $oneUnit."px");
+		self::addProperty('.u2, .u2 img','width', $twoUnits."px");
+		self::addProperty('.u3, .u3 img, header','width', $threeUnits."px");
+		
+		$title = $stylesSettings->style->title;
+
+		if($title->alignment != 'default')		
+		self::addProperty('header', 'text-align', $title->alignement); 
+		if($title->font != 'default')		
+		self::addProperty('header', 'font-family', $title->font); 
+		if($title->size != 'default')		
+		self::addProperty('header', 'font-size', $title->size."px"); 	
+		
+		$nav = $stylesSettings->style->navigation;
+
+		if($nav->alignment != 'default')		
+		self::addProperty('navigation', 'text-align', $nav->alignement); 
+		if($nav->font != 'default')		
+		self::addProperty('navigation', 'font-family', $nav->font); 
+		if($nav->size != 'default')		
+		self::addProperty('navigation', 'font-size', $nav->size."px"); 	
+		
+		$error = $stylesSettings->style->error;
+
+		if($error->alignment != 'default')		
+		self::addProperty('.error', 'text-align', $error->alignment); 
+		if($error->font != 'default')		
+		self::addProperty('.error', 'font-family', $error->font); 
+		if($error->size != 'default')		
+		self::addProperty('.error', 'font-size', $error->size."px"); 
+		
+		self::addProperty('.footer', 'opacity',$stylesSettings->style->footer->opacity); 		
 
 		return self::renderFile();
 	}
+	
+	private static function tabToRGB($tab){
+	  if(count($tab) == 4){
+	  	return "rgba(".$tab[0].",".$tab[1].",".$tab[2].",".$tab[3].")";
+	  }else{
+	  	return "rgb(".$tab[0].",".$tab[1].",".$tab[2].")";
+	  }
+	}
+	
+	private static function tabToShadow($tab){
+		return $tab[0]."px ".$tab[1]."px ".$tab[2]."px ".self::tabToRGB($tab[3]);
+  }
 
 	private static function addProperty($selector, $property, $value, $putVendors = false, $vendorOnValue = false){
 		if($putVendors){
