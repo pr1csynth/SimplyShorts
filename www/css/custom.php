980f54b3753abbd8fa780356798a54bf36f513ff
Le Chat Léon <contact@lechatleon.com>
2012-11-06 19:34:30 +0100

    Add custom.php to gen up to date custom css.

---
+++ www/css/custom.php
@@ -0,0 +1,13 @@
+<?php
+/*
+## FILE : custom.php
+When debug mode is active, this php code return up to date customisation css.
+*/
+
+require '../lib/ConfigFilesParser.class.php';
+require '../lib/CSSComputer.class.php';
+
+$stylesSettings = ConfigFilesParser::parse("../config/styles");
+echo CSSComputer::genCustomisation($stylesSettings);
+
+?>
