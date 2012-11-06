33fcc098199d623293324fdbfda3eb0e93f4e8d2
Le Chat Léon <contact@lechatleon.com>
2012-11-06 18:15:20 +0100

    Add errors managment.

--- www/index.php
+++ www/index.php
@@ -14,6 +14,7 @@ define('ROOTPATH',realpath('.')."/");
 
 try{
     $SimplyShorts = new SimplyShorts;
+    $SimplyShorts->renderPage();
 }catch(Exception $e){
     echo "FATAL ERROR: ".$e->getMessage();
 }
--- www/lib/SimplyShorts.class.php
+++ www/lib/SimplyShorts.class.php
@@ -5,9 +5,11 @@ Main Program.
 */
 
 class SimplyShorts{
+
+	private $pageComputer; 
 	
 	public function __construct(){
-		
+
 		try{
 			$config = ConfigFilesParser::parse("config/config");
 		}catch(Exception $e){
@@ -16,9 +18,42 @@ class SimplyShorts{
 		}
 
 		self::defineConstants($config);
+
+		if(DEBUG){
+			ini_set('display_errors', 1);
+			error_reporting(E_ALL);	
+			echo "◰";
+		}
+
+
+	}
+
+	public function renderPage(){
+
 	}
 
 	private function defineConstants($config){
+		$ss = $config->ss;
+		define("ONLINE", $ss->online);
+		define("DEBUG", $ss->debug);
+		define("OFFLINEMSG", $ss->offlineMessage);
+		
+		define("SITENAME",$ss->name);
+		define('SITEDESC', $ss->description);
+
+		define('AFFHEADER', $ss->header);
+		define('AFFNAV',$ss->navigation);
+		define('AFFMAXBLOCKS', $ss->maxBlocks);
+		
+		define('DISPLAYNAME',$ss->displayName);
+
+		if($ss->url->urlRewrite){
+			define('BASEURL', $ss->url->basedir."");
+		}else{
+			define('BASEURL', $ss->url->basedir."?");
+		}
+
+		define('SEPARATOR', $ss->url->separator);
 		
 	}
 }
