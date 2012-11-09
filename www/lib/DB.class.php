a4307c86425d99113f47cb872b2c66604cc52b34
Le Chat Léon <contact@lechatleon.com>
2012-11-09 12:05:44 +0100

    Add Basic DB management

--- www/lib/DB.class.php
+++ www/lib/DB.class.php
@@ -7,10 +7,60 @@ DB manager.
 class DB{
 
 	private $db;
+	private $prefix;
 
 	public function __construct($params){
-		throw new Exception("No db.");
-		
+		$options = array(	PDO::MYSQL_ATTR_INIT_COMMAND    => "SET NAMES utf8" ,
+							PDO::ATTR_PERSISTENT => $params->persitent
+						);
+
+		try{
+			$this->db = new PDO('mysql:host='.$params->host.';dbname='.$params->base, $params->user, $params->password, $options);
+		}catch(Exception $e){
+			throw new Exception("DB connection error : ".$e->getMessage()." (error ".$e->getCode().")", 1, $e);
+		}	
+
+		$this->prefix = $params->prefix;	
+
+		$count = $this->query('SELECT * FROM '.$this->prefix.'blocks');
+
+		if($count == array()){
+			throw new Exception("Not installed or bad prefix", 1);
+		}
+
+	}
+
+	private function query($queryString, $params = array()){
+		try{
+			$query = $this->db->prepare($queryString);
+			foreach ($params as $key => $value) {
+				$query->bindParam(':'.$key, $value, $this->PDOTypeOf($value));
+			}
+			$query->execute();
+
+			$results = $query->fetchAll(PDO::FETCH_ASSOC);
+
+			$query->closeCursor();
+
+			return $results;
+		}catch(Exception $e){
+			throw new Exception("Query error : ".$e->getMessage, 1, $e);
+			return null;
+		}
+	}
+
+	private function PDOTypeOf($var){
+		if(is_int($var))
+			$param = PDO::PARAM_INT;
+		elseif(is_bool($var))
+			$param = PDO::PARAM_BOOL;
+		elseif(is_null($var))
+			$param = PDO::PARAM_NULL;
+		elseif(is_string($var))
+			$param = PDO::PARAM_STR;
+		else
+			$param = PDO::PARAM_STR;	
+		return $param;
 	}
 
 }
